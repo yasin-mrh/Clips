56919f2fb04e9e7f7800c5f0c4e71a35d1996be1
Seyed Yasin Mirhashemi <yasin.st2022@gmail.com>
2026-02-15 00:40:05 +0330

    Feat– Added 'styleTab' function to dynamically update the style of tabs anchors

--- src/app/shared/tabs-container/tabs-container.ts
+++ src/app/shared/tabs-container/tabs-container.ts
@@ -1,22 +1,35 @@
 import { Component, contentChildren, AfterContentInit } from '@angular/core';
 import { Tab } from '../tab/tab';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-tabs-container',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './tabs-container.html',
   styleUrl: './tabs-container.css',
 })
 export class TabsContainer implements AfterContentInit {
   tabs = contentChildren(Tab);
-
-  selectTab(tab: Tab) {
-    this.tabs().forEach(tab => tab.active.set(false));
-    tab.active.set(true);
-  }
-
+  
   ngAfterContentInit(): void {
     this.selectTab(this.tabs()[0]);
   }
-}
+  
+  selectTab(tab: Tab, e?: Event) {
+    this.tabs().forEach(tab => tab.active.set(false));
+    tab.active.set(true);
 
+    e?.preventDefault();
+  }
+
+
+  styleTab(tab: Tab) {
+    const activeTabStyle = 'bg-indigo-800';
+    const passiveTabStyle = 'bg-indigo-400 hover:transition hover:bg-white hover:text-indigo-400';
+    
+    return {
+      [activeTabStyle]: tab.active(),
+      [passiveTabStyle]: !tab.active()
+    };
+  };
+}
